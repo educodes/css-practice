10193a184436e0d6081dadce78d91f8486333234
Eduardo Pérez <educodes0@gmail.com>
2021-06-15 22:55:49 -0500

    Animation example 04

---
+++ examples/ex04/script.js
@@ -0,0 +1,20 @@
+
+
+let obscro2 = {
+  delay: 1000,
+  duration: 1000,
+  distance: "80px",
+  origin: "left",
+};
+
+ScrollReveal().reveal(".form", obscro2);
+
+
+let obscro = {
+  delay: 1000,
+  duration: 1000,
+  distance: "80px",
+  origin: "top",
+};
+
+ScrollReveal().reveal(".note", obscro);
